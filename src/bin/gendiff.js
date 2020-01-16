098025dc7a589c26fe0590c14cc8d2a675cf3e92
talita <polikashina@gmail.com>
2020-01-16 12:39:33 +0100

    add 'format' option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,10 +2,30 @@
 
 import program from 'commander';
 
+
+let firstConfig;
+let secondConfig;
+
 // const program = new commander.Command();
-program.version('0.0.1');
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstCongif> <secondConfig>')
+  .action((firstConf, secondConf) => {
+    firstConfig = firstConf;
+    secondConfig = secondConf;
+  });
 
 program.parse(process.argv);
 
-console.log('Hello!');
+if (typeof firstConfig === 'undefined') {
+  console.error('error: no configs name given!');
+  process.exit(1);
+}
+
+console.log('first config:', firstConfig);
+console.log('second config:', secondConfig);
+if (program.format) {
+  console.log('format', program.format);
+}
